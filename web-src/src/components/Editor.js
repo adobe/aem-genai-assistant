0e8cfe4db7dc855b4b66ba81ab4111c78a4c2764
Vitaly Tsaplin <tsaplin@adobe.com>
2023-10-30 23:21:55 +0100

    feat: evaluate expressions (#2)

--- web-src/src/components/Editor.js
+++ web-src/src/components/Editor.js
@@ -9,14 +9,62 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import {
-  TextArea, ComboBox, Item, ButtonGroup, Button, Grid,
+  TextArea, Item, ButtonGroup, Button, Grid, Picker, Flex, NumberField, ToggleButton,
 } from '@adobe/react-spectrum';
+import wretch from 'wretch';
 import { useApplicationContext } from './ApplicationProvider.js';
 
+function replaceTemplateStrings(str, valuesMap) {
+  return str.replace(/{\s*([^{}\s]+)\s*}/g, (match, key) => {
+    return key in valuesMap ? valuesMap[key] : match;
+  });
+}
+
+function renderPrompt(prompt, segment, variationCount, sourceView) {
+  return sourceView ? prompt : replaceTemplateStrings(prompt, { segment, num: variationCount });
+}
+
 function Editor() {
-  const { completionService } = useApplicationContext();
+  const { websiteUrl, completionService } = useApplicationContext();
+
+  const [promptTemplates, setPromptTemplates] = React.useState([]);
+  const [segments, setSegments] = React.useState([]);
+  const [segment, setSegment] = React.useState('');
+  const [variationCount, setVariationCount] = React.useState(1);
+  const [sourceView, setSourceView] = React.useState(false);
+  const [prompt, setPrompt] = React.useState('');
+
+  useEffect(() => {
+    // Fetch prompt templates
+    wretch(`${websiteUrl}/prompttemplates.json`).get().json((json) => {
+      setPromptTemplates(json.data.map((row) => ({
+        label: row.Label,
+        template: row['Prompt Template'],
+      })));
+    }).catch((error) => {
+      console.log(error);
+    });
+    // Fetch segments
+    wretch(`${websiteUrl}/segments.json`).get().json((json) => {
+      setSegments(json.data.map((row) => ({
+        label: row.Label,
+        segment: row.Segment,
+      })));
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, []);
+
+  const promptSelectionHandler = useCallback((selected) => {
+    setPrompt(promptTemplates[selected].template);
+  }, [promptTemplates]);
+
+  const segmentSelectionHandler = useCallback((selected) => {
+    setSegment(segments[selected].segment);
+  }, [segments]);
+
   return (
     <Grid
       columns={['1fr']}
@@ -24,12 +72,21 @@ function Editor() {
       gap="size-200"
       margin={0}
       width="100%" height="100%">
-      <ComboBox label="Prompt Template">
-        <Item key="prompt1">Prompt 1</Item>
-        <Item key="prompt2">Prompt 2</Item>
-        <Item key="prompt3">Prompt 3</Item>
-      </ComboBox>
-      <TextArea width="100%" height="100%" />
+      <Flex direction="row" gap="size-200" alignItems={'end'}>
+        <Picker label="Prompt Template" isLoading={!promptTemplates} placeholder="" onSelectionChange={promptSelectionHandler}>
+          {promptTemplates ? promptTemplates
+            .map((template, index) => <Item key={index}>{template.label}</Item>) : []}
+        </Picker>
+        <Picker label="Segment" isLoading={!segments} placeholder="" onSelectionChange={segmentSelectionHandler}>
+          {segments ? segments
+            .map((seg, index) => <Item key={index}>{seg.label}</Item>) : []}
+        </Picker>
+        <NumberField label="Number Of Variants" defaultValue={4} minValue={1} maxValue={4} onChange={setVariationCount} />
+        <ToggleButton isSelected={sourceView} onChange={setSourceView}>Source</ToggleButton>
+      </Flex>
+      <TextArea width="100%" height="100%"
+                value={renderPrompt(prompt, segment, variationCount, sourceView)}
+                onChange={setPrompt}/>
       <ButtonGroup>
         <Button variant="primary" onPress={() => console.log(completionService.complete('prompt', 0.1))}>Generate</Button>
       </ButtonGroup>

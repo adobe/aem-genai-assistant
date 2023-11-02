124ea0a17bbc4e87f208b7673808b8ca97b1e89d
Vitaly Tsaplin <tsaplin@adobe.com>
2023-11-02 12:58:17 +0100

    feat: implement firefall proxy

--- actions/complete/index.js
+++ actions/complete/index.js
@@ -1,68 +1,80 @@
-const wretch = require('wretch')
-const FormDataAddon = require('wretch/addons/formData')
-const retry = require('wretch/middlewares/retry')
-
-const GPT_35_TURBO = 'gpt-35-turbo'
-const GPT_4 = 'gpt-4'
+const wretch = require('wretch');
+const FormDataAddon = require('wretch/addons/formData');
 
 async function getAccessToken(params) {
-  const IMSS_ENDPOINT = 'https://ims-na1.adobelogin.com'
-  const IMSS_CLIENT_ID = params.IMSS_CLIENT_ID
-  const IMSS_CLIENT_SECRET = params.IMSS_CLIENT_SECRET
-  const IMSS_SERVICE_PERMANENT_AUTHORIZATION_CODE = params.IMSS_SERVICE_PERMANENT_AUTHORIZATION_CODE
+  const endpoint = params['IMS_ENDPOINT']
+  const clientId = params['IMS_CLIENT_ID']
+  const clientSecret = params['IMS_CLIENT_SECRET']
+  const pac = params['IMS_PAC']
 
-  const imssAccessTokenResponse = await wretch(IMSS_ENDPOINT + '/ims/token/v1')
-    .post({
-      client_id: IMSS_CLIENT_ID,
-      client_secret: IMSS_CLIENT_SECRET,
-      grant_type: 'authorization_code',
-      code: IMSS_SERVICE_PERMANENT_AUTHORIZATION_CODE
+  const json = await wretch(endpoint + '/ims/token/v2')
+    .addon(FormDataAddon).formData({
+      client_id: clientId,
+      client_secret: clientSecret,
+      grant_type: 'client_credentials',
+      scope: 'openid,AdobeID,read_organizations',
     })
+    .post()
+    .json();
 
-  return imssAccessTokenResponse.json().access_token
+  return json['access_token'];
 }
 
-async function completion(params, accessToken, prompt, llm_metadata) {
-  const FIREFALL_ENDPOINT = 'https://firefall.adobe.io'
-  const FIREFALL_API_KEY = params.FIREFALL_API_KEY
-  const FIREFALL_IMSS_ORG = params.FIREFALL_IMSS_ORG
+async function completion(params, accessToken, prompt, model, temperature) {
+  const endpoint = params['FIREFALL_ENDPOINT']
+  const apiKey = params['FIREFALL_API_KEY']
+  const org = params['FIREFALL_IMS_ORG']
 
-  const response = await wretch(FIREFALL_ENDPOINT + '/v1/completions')
+  console.log(endpoint)
+  console.log(apiKey)
+  console.log(org)
+
+  return await wretch(endpoint + '/v1/completions')
     .headers({
-      Authorization: `Bearer ${accessToken}`,
-      'x-api-key': FIREFALL_API_KEY,
-      'x-gw-ims-org-id': FIREFALL_IMSS_ORG,
+      'x-gw-ims-org-id': org,
+      'x-api-key': apiKey,
+      'Authorization': `Bearer ${accessToken}`,
       'Content-Type': 'application/json'
     })
     .post({
       dialogue: {
         question: prompt
       },
-      llm_metadata: llm_metadata
+      llm_metadata: {
+        llm_type: 'azure_chat_openai',
+        model_name: model,
+        temperature: temperature,
+        max_tokens: 4096,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        n: 1
+      }
     })
-    .json()
-
-  return response
+    .json();
 }
 
 async function main (params) {
-  const accessToken = await getAccessToken(params)
-
-  const llm_metadata = {
-    llm_type: 'azure_chat_openai',
-    model_name: GPT_35_TURBO,
-    temperature: 1,
-    max_tokens: 4096,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    n: 1
+  try {
+    const accessToken = await getAccessToken(params);
+    const prompt = params['prompt'] || 'Who are you?';
+    const model = params['model'] || 'gpt-4';
+    const temperature = params['t'] || 0.0;
+    const json = await completion(params, accessToken, prompt, model, temperature);
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: json
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      headers: { 'Content-Type': 'application/json' },
+      body: {
+        error: e.message
+      }
+    };
   }
-
-  prompt = "Hello, World"
-  const response = await completion(params, accessToken, prompt, llm_metadata)
-
-  return { body: response.json() };
 }
 
 exports.main = main

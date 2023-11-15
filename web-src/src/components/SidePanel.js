9b9c26cbee95e0e9ec2d7c9d696230df37bb68b6
Tobias Reiss <reiss@adobe.com>
2023-11-15 22:02:05 +0100

    add font family, faq and copyright

--- web-src/src/components/SidePanel.js
+++ web-src/src/components/SidePanel.js
@@ -1,19 +1,49 @@
-import {Flex, Grid, Image, Link, Text} from '@adobe/react-spectrum';
-import React, {useCallback} from 'react';
-import {useApplicationContext} from './ApplicationProvider.js';
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import {
+  Flex, Grid, Image, Link, Text,
+} from '@adobe/react-spectrum';
+import React, { useCallback } from 'react';
+import { css } from '@emotion/css';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { useApplicationContext } from './ApplicationProvider.js';
 
 import LogoIcon from '../assets/logo.svg';
 import HelpIcon from '../assets/help.svg';
-import {useRecoilState, useRecoilValue} from 'recoil';
-import {sessionsState} from '../state/SessionsState.js';
-import {currentSessionState} from '../state/CurrentSessionState.js';
-import {ViewType, viewTypeState} from '../state/ViewType.js';
+import { sessionsState } from '../state/SessionsState.js';
+import { currentSessionState } from '../state/CurrentSessionState.js';
+import { ViewType, viewTypeState } from '../state/ViewType.js';
 
 export function SidePanel(props) {
   const { appVersion } = useApplicationContext();
   const sessions = useRecoilValue(sessionsState);
   const [currentSession, setCurrentSession] = useRecoilState(currentSessionState);
   const [viewType, setViewType] = useRecoilState(viewTypeState);
+
+  const style = {
+    help: css`
+      /* Heading/Sans/Sans Default XXS */
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 18px; /* 128.571% */
+    `,
+    copyright: css`
+      font-size: 10px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 18px; /* 180% */
+    `,
+  };
 
   const handleRecent = useCallback((session) => {
     setCurrentSession(session);
@@ -52,13 +82,10 @@ export function SidePanel(props) {
 
       <Flex direction={'column'} gridArea={'footer'} gap={10}>
         <Flex direction={'row'} justifyContent={'start'} alignItems={'center'} gap={'size-100'}>
-          <Image src={HelpIcon} width={'20px'}/><Link>Help</Link>
+          <Image src={HelpIcon} width={'20px'}/><Link href="https://www.aem.live/developer/configuring-aem-genai-assistant-sidekick-plugin" target="_blank" UNSAFE_className={style.help}>Help & FAQ</Link>
         </Flex>
-        <Flex direction={'row'} justifyContent={'start'} alignItems={'center'} gap={'size-100'}>
-          <Image src={HelpIcon} width={'20px'}/><Link>Terms and legals</Link>
-        </Flex>
-        <Text UNSAFE_style={{ fontSize: '10px' }}>Copyright © 2023 Adobe. All rights reserved</Text>
+        <Text UNSAFE_className={style.copyright}>Copyright © 2023 Adobe. All rights reserved</Text>
       </Flex>
     </Grid>
-  )
+  );
 }

ec344a14b66efa6b7be1a029efc580d410fede0c
Dragos Dascalita Haut <ddascal@adobe.com>
2023-12-05 12:59:18 -1000

    feat: persist favorites in a remote storage

---
+++ actions/favorite/index.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+const { asGenericAction } = require('../GenericAction.js');
+const { asAuthAction } = require('../AuthAction.js');
+
+const { AppError } = require('../../web-src/src/helpers/ErrorMapper.js');
+const { wretchRetry } = require('../Network.js');
+
+async function main(params) {
+    const { data } = params;
+    try {
+        // data is an array of objects,
+        // iterate over each object and make a POST reuest to the API endpoing
+        // https://hook.app.workfrontfusion.com/cytq82vrakjn5p8xtaashn6gm2qc3jif
+        // with the data object as the body
+        // return the response from the API
+        console.log('Favorites to be stored:', data);
+        results = []
+
+        for (const variant of data) {
+            console.log('Variant to be stored:', variant);
+            r = await wretchRetry(`https://hook.app.workfrontfusion.com/cytq82vrakjn5p8xtaashn6gm2qc3jif`)
+                .headers({
+                    'Content-Type': 'application/json',
+                })
+                .post({
+                    data: variant
+                })
+                .json();
+            results.push(r);
+        }
+        
+        return {
+            statusCode: 200,
+            body: {
+                message: 'Favorites updated in API',
+                results: results
+            }
+        };
+    } catch (error) {
+        throw new AppError(error, 'FUSION');
+    }
+}
+
+//exports.main = asAuthAction(asGenericAction(main));
+exports.main = asGenericAction(main);
---
+++ web-src/src/state/PersistentRemoteStorageEffect.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { useApplicationContext } from "../components/ApplicationProvider";
+
+export const createRemotePersistenceEffect = key => ({ onSet }) => {
+    const { remoteFavoritesService } = useApplicationContext();
+    onSet(newValue => {
+        // if newValue's array length is 0, then do return
+        if (newValue.length === 0) {
+            return;
+        }
+
+        console.log('Favorites to be stored:', newValue);
+        remoteFavoritesService.save_favorite(newValue)
+            //   fetch('https://hook.app.workfrontfusion.com/cytq82vrakjn5p8xtaashn6gm2qc3jif', {
+            //     method: 'POST',
+            //     headers: {
+            //       'Content-Type': 'application/json',
+            //     },
+            //     body: {
+            //         data: data
+            //     },
+            //  })
+            .then(response => response)
+            .then(data => console.log('Favorites updated in API:', data))
+            .catch(error => console.error('Error updating favorites:', error));
+    });
+};

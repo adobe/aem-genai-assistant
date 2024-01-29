079e4c31a50732a8e93ca7bad50653db33c20b19
Vitaly Tsaplin <tsaplin@adobe.com>
2024-01-30 00:18:08 +0100

    chore: remove api key

--- bash.sh
+++ bash.sh
@@ -1,9 +1,9 @@
 curl https://mc.adobe.io/sitesinternal/target/audiences \
    -H "Accept: application/vnd.adobe.target.v3+json" \
-   -H "X-Api-Key: 5090f9f1be114511944e36b6e640170c" \
+   -H "X-Api-Key: {}" \
    -H "Authorization: Bearer {}" | jq
 
 curl https://mc.adobe.io/sitesinternal/target/audiences/2048630 \
    -H "Accept: application/vnd.adobe.target.v3+json" \
-   -H "X-Api-Key: 5090f9f1be114511944e36b6e640170c" \
+   -H "X-Api-Key: {}" \
    -H "Authorization: Bearer {}" | jq

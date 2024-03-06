c37d05a11a6f93737e0a2aeb9f7482290f7728a7
Ashish Kayastha <akayastha@adobe.com>
2024-03-06 17:00:09 -0600

    chore: Code cleanup

--- web-src/src/components/FavoriteVariantCard.js
+++ web-src/src/components/FavoriteVariantCard.js
@@ -133,7 +133,6 @@ export function FavoriteVariantCard({ variant, ...props }) {
                 width="size-2000"
                 variant="secondary"
                 style="fill"
-                // onPress={() => handleGenerateImage('')}
                 onPress={() => handleGenerateImagePrompt()}
                 isDisabled={!isExpressAuthorized}>
                 {imagePromptProgress ? <ProgressCircle size="S" aria-label="Generate" isIndeterminate right="8px" /> : <GenAIIcon marginEnd={'8px'} />}

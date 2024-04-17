fa143624f19d013fe9a2b84ba63fff3a8bfb3cd8
Audrey Kho <akho@adobe.com>
2024-04-17 14:25:54 -0700

    feat: add complete-json action

---
+++ actions/complete-json/index.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+const { asGenericAction } = require('../GenericAction.js');
+const { asAuthAction } = require('../AuthAction.js');
+const { asFirefallAction } = require('../FirefallAction.js');
+
+async function main(params) {
+  const {
+    prompt, temperature, model, firefallClient,
+  } = params;
+  return firefallClient.completionJson(prompt ?? 'Who are you?', temperature ?? 0.0, model ?? 'gpt-4-turbo');
+}
+
+exports.main = asGenericAction(asAuthAction(asFirefallAction(main)));

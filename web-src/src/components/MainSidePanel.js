580fe56407c0ebd7f4c6977e9959ed69f7f77380
Serhii Litviachenko <slitviachenko@gmail.com>
2024-05-02 09:31:27 -0500

    Move the version number into Copyright

--- web-src/src/components/MainSidePanel.js
+++ web-src/src/components/MainSidePanel.js
@@ -18,6 +18,7 @@ import { css } from '@emotion/css';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { useIntl } from 'react-intl';
 
+import { intlMessages as appIntlMessages } from './App.l10n.js';
 import { intlMessages } from './MainSidePanel.l10n.js';
 import { useApplicationContext } from './ApplicationProvider.js';
 import { USER_GUIDELINES_URL } from './LegalTermsLink.js';
@@ -51,11 +52,6 @@ export function MainSidePanel(props) {
       font-size: 18px;
       font-style: normal;
       font-weight: 700;
-    `,
-    versionTag: css`
-      background-color: #E9E9E9;
-      padding: 0px 9px;
-      border-radius: 7px;
     `,
     menu: css`
       font-size: 14px;
@@ -185,10 +181,16 @@ export function MainSidePanel(props) {
             <ClickableImage src={FileTxt} width={'20px'} title={formatMessage(intlMessages.mainSidePanel.userGuidelinesMenuItem)} alt={'Guidelines'} onClick={() => window.open(USER_GUIDELINES_URL, '_blank')} />
             {mainSidePanel === MainSidePanelType.Expanded && <Link href={USER_GUIDELINES_URL} target="_blank" UNSAFE_className={style.menu}>{formatMessage(intlMessages.mainSidePanel.userGuidelinesMenuItem)}</Link>}
           </li>
-          {mainSidePanel === MainSidePanelType.Expanded
-            ? <Text UNSAFE_className={style.copyright}>{formatMessage(intlMessages.mainSidePanel.copyrightLabel)}</Text>
-            : <Text />}
         </ul>
+        {mainSidePanel === MainSidePanelType.Expanded
+          ? <>
+            <Text UNSAFE_className={style.copyright}>
+              {formatMessage(intlMessages.mainSidePanel.copyrightLabel)}
+              <br/>
+              {`${formatMessage(appIntlMessages.app.name)}, v${appVersion}`}
+            </Text>
+          </>
+          : <Text />}
       </Flex>
     </Grid>
   );

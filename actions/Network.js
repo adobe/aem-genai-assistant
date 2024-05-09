fb372b32a5c4e5c8604800548e00ae299ff401a2
Serhii Litviachenko <slitviachenko@gmail.com>
2024-05-09 17:43:01 -0500

    Delete unneeded REQUEST_TIMEOUT for backend requests

--- actions/Network.js
+++ actions/Network.js
@@ -9,7 +9,6 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-const AbortAddon = require('wretch/addons/abort');
 
 const wretch = require('wretch');
 const { retry } = require('wretch/middlewares/retry');
@@ -18,8 +17,6 @@ const { WretchError } = require('wretch');
 const { Core } = require('@adobe/aio-sdk');
 
 const logger = Core.Logger('FirefallAction');
-
-const REQUEST_TIMEOUT = 55 * 1000;
 
 function createWretchError(status, message) {
   const error = new WretchError();
@@ -31,14 +28,8 @@ function createWretchError(status, message) {
 function wretchWithOptions(url, shouldRetry = false) {
   return wretch(url)
     .middlewares(shouldRetry ? [retry()] : [])
-    .addon(AbortAddon())
-    .resolve((resolver) => resolver.setTimeout(REQUEST_TIMEOUT))
     .resolve((resolver) => {
       return resolver.fetchError((error) => {
-        if (error.name === 'AbortError') {
-          logger.error('Request aborted', error);
-          throw createWretchError(408, 'Request timed out');
-        }
         logger.error('Network error', error);
         throw createWretchError(500, 'Network error');
       });

580884ea9a4ded3f4d2aeeacbf1067aa6d0cba19
Vitaly Tsaplin <tsaplin@adobe.com>
2024-07-24 09:18:36 +0200

    chore: ci/cd fails

--- actions/scraper/index.js
+++ actions/scraper/index.js
@@ -9,7 +9,6 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-const Papa = require('papaparse');
 const { JSDOM } = require('jsdom');
 const wretch = require('../Network.js');
 const { asGenericAction } = require('../GenericAction.js');

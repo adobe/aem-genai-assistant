674856cbb91c03db4d86ef0d6bab24d2849201a7
Audrey Kho <59587534+audrey-kho@users.noreply.github.com>
2026-04-01 10:35:51 -0700

    [SITES-22179] fix: use localized string for Help icon in MainSidePanel (#320)
    * fix: use localized string for Help icon in MainSidePanel
    
    * fix: accidental overwrite due to merge conflict

--- web-src/src/components/MainSidePanel.js
+++ web-src/src/components/MainSidePanel.js
@@ -240,7 +240,7 @@ export function MainSidePanel(props) {
       <Flex direction={'column'} gridArea={'footer'} gap={'16px'}>
         <div className={style.menu}>
           <div className={style.menuItem}>
-            <ClickableImage src={HelpIcon} width={'20px'} title={formatMessage(intlMessages.mainSidePanel.helpAndFaqsMenuItem)} alt={'Help'} onClick={() => window.open(formatMessage(intlMessages.mainSidePanel.helpUrl), '_blank')} />
+            <ClickableImage src={HelpIcon} width={'20px'} title={formatMessage(intlMessages.mainSidePanel.helpAndFaqsMenuItem)} alt={formatMessage(intlMessages.mainSidePanel.helpAndFaqsAltText)} onClick={() => window.open(formatMessage(intlMessages.mainSidePanel.helpUrl), '_blank')} />
             {mainSidePanelType === MainSidePanelType.Expanded && <Link href={formatMessage(intlMessages.mainSidePanel.helpUrl)} target="_blank" UNSAFE_className={style.menu}>{formatMessage(intlMessages.mainSidePanel.helpAndFaqsMenuItem)}</Link>}
           </div>
           <div className={style.menuItem}>
